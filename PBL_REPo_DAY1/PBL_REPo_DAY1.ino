dc9da03f04e2b6d2030db8597ee4f604761d57d9
boris-k-eet <boris.kosenko@dawsoncollege.qc.ca>
2025-11-25 12:28:47 -0500

    added comment to show oled pins

--- PBL_REPo_DAY1/PBL_REPo_DAY1.ino
+++ PBL_REPo_DAY1/PBL_REPo_DAY1.ino
@@ -6,11 +6,14 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
+// OLED pins
 #define OLED_MOSI   23
 #define OLED_CLK   18
 #define OLED_DC    16
 #define OLED_CS    5
 #define OLED_RESET 19
+
+
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
   OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
